94d4cd27ae818f11a4ae2e94ed5180c70a272c33
Ben Adams <darwinzero@gmail.com>
2023-03-06 14:17:59 +1300

    minor edits

--- src/evaporate.rs
+++ src/evaporate.rs
@@ -5,7 +5,7 @@ pub const PREFIX_EVAPORATE_BYTE: &[u8] = b"__evaporatebyte__";
 pub static EVAPORATE_BYTE: Item<u8> = Item::new(PREFIX_EVAPORATE_BYTE);
 
 pub fn evaporate_gas(store: &mut dyn Storage, multiplier: u32) -> StdResult<()> {
-    for i in 0..multiplier {
+    for _ in 0..multiplier {
         EVAPORATE_BYTE.save(store, &0_u8)?;
     }
     Ok(())
