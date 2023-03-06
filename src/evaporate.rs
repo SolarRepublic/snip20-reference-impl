b13301507073988ed2536c0feefa4634381d97ad
Ben Adams <darwinzero@gmail.com>
2023-03-06 13:30:12 +1300

    dev: change evaporate byte key

--- src/evaporate.rs
+++ src/evaporate.rs
@@ -1,14 +1,14 @@
 use cosmwasm_std::{Storage, StdResult};
 use secret_toolkit::storage::Item;
 
-pub const PREFIX_BURN_BYTE: &[u8] = b"__burnbyte__";
-pub static BURN_BYTE: Item<u8> = Item::new(PREFIX_BURN_BYTE);
+pub const PREFIX_EVAPORATE_BYTE: &[u8] = b"__evaporatebyte__";
+pub static EVAPORATE_BYTE: Item<u8> = Item::new(PREFIX_EVAPORATE_BYTE);
 
 pub fn evaporate_gas(store: &mut dyn Storage, multiplier: u32) -> StdResult<()> {
     for _ in 0..multiplier {
         //if u64::from(i) >= u64::MAX {
             // never executes but compiler will not optimize away for loop
-            BURN_BYTE.save(store, &0_u8)?;
+            EVAPORATE_BYTE.save(store, &0_u8)?;
         //}
     }
     Ok(())
